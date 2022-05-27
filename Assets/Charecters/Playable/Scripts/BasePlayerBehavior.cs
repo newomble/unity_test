5de1e9a8d0f8166bf637b89096d7202bd4cf217e
Nick Womble <32205618+newomble@users.noreply.github.com>
2022-05-27 14:40:38 -0400

    added movement (#1)

--- Assets/Charecters/Playable/Scripts/BasePlayerBehavior.cs
+++ Assets/Charecters/Playable/Scripts/BasePlayerBehavior.cs
@@ -1,18 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class BasePlayerBehavior : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float movespeed = 1f;
+    Rigidbody2D rb;
+    Vector2 directionalInput;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnMove(InputValue moveValue)
     {
-        
+        directionalInput = moveValue.Get<Vector2>();
+    }
+
+    private void FixedUpdate()
+    {
+        if (this.hadMovementInput())
+        {
+            Vector2 nextPosition = this.calculateNextPosition();
+            if (this.canMoveTo(nextPosition))
+            {
+                rb.MovePosition(nextPosition);
+            }
+        }
+    }
+
+    private bool hadMovementInput()
+    {
+        return directionalInput != Vector2.zero;
+    }
+
+    private bool canMoveTo(Vector2 nextPosition)
+    {
+        //TODO
+        return true;
+    }
+
+    private Vector2 calculateNextPosition()
+    {
+        return rb.position + movespeed * Time.fixedDeltaTime * directionalInput;
     }
 }
